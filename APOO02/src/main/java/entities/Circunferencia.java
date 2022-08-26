46c0de76e75c6e208b8dd269f9f319e3071707b3
pablilloab <pab.a.bari@gmail.com>
2022-08-26 15:06:07 -0300

    Segundo ejercicios aprendizaje

---
+++ APOO02/src/main/java/entities/Circunferencia.java
@@ -0,0 +1,23 @@
+package entities;
+
+import java.util.Scanner;
+
+public class Circunferencia {
+    private double radio;
+
+    public Circunferencia(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public void crearCircunferencia (double radio){
+        this.setRadio(radio);
+    }
+}
